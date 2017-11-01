60397527a53d258b537e0a3d9ff531fab634126b
Amy Simmons <asimmons@twitter.com>
2017-11-01 23:12:09 +0000

    with jquery

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var header = $('#title');
+
+console.log(header);
+
+var list  = $('ul')
+
+console.log(list);
+
+var listItems = $('li');
+
+console.log(listItems);
+
+var importantListItem = $('li.important')
+
+console.log(importantListItem);
+
+
+
+
